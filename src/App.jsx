899333e3d620bdec5fdcda4f995a90e4863ccc02
daikikejics <cskejidroid@gmail.com>
2024-12-13 21:15:12 +0900

    5-issueの確認

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className="text-3xl text-red-500">Vite + React + Test + Test5</h1>
+      <h1 className="text-3xl text-red-500">Vite + React + Test + 5-issue</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
